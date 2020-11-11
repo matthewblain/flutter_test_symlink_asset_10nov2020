84009bdc079372a09a4fc8c5e4adcbc015d6636c
matthewblain <mb7oo@hotmail.com>
2020-11-10 20:35:14 -0800

    Page title now loads from an asset
    The page title loads from an asset.
    The asset is just a file in the source repository for now.
    Everything works at this stage.

--- test_symlink_asset_10nov2020/README.md
+++ test_symlink_asset_10nov2020/README.md
@@ -4,13 +4,8 @@ Testing Flutter loading assets from a symlink
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is copy of the default a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+All it does differently is load the page title from an asset text file.
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+The purpose of this project is to test building issues on Windows.
--- test_symlink_asset_10nov2020/lib/main.dart
+++ test_symlink_asset_10nov2020/lib/main.dart
@@ -4,10 +4,15 @@ void main() {
   runApp(MyApp());
 }
 
+Future<String> loadAsset(BuildContext context, String path) async {
+  return await DefaultAssetBundle.of(context).loadString(path);
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var pageTitle = loadAsset(context, 'assets/page_title.txt');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -26,24 +31,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -73,10 +67,14 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+        // Load title from assets.
+          title: FutureBuilder(
+              future: DefaultAssetBundle.of(context)
+                  .loadString('assets/page_title.txt'),
+              builder: (context, snapshot) {
+                return Text(
+                    snapshot.data == null ? "Not Loaded" : snapshot.data.trim());
+              })),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.

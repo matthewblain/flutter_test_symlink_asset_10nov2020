efb9d8ba69c4b5547b33dbd9c69ca62614080eda
matthewblain <mb7oo@hotmail.com>
2020-11-10 20:18:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_test_symlink_asset_10nov2020
+ 
---
+++ test_symlink_asset_10nov2020/android/app/src/main/kotlin/com/mblain/test_symlink_asset_10nov2020/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mblain.test_symlink_asset_10nov2020
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
